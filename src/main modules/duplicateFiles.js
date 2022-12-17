cb8b5f3ae02df5dd70cc30c136d15d1c345ecc34
markuskreukniet <shonof@gmail.com>
2022-12-17 23:39:32 +0100

    refactor

---
+++ src/main modules/duplicateFiles.js
@@ -0,0 +1,47 @@
+const crypto = require("crypto");
+
+function determineDuplicateFilesByHash(paths) {
+  const pathHashCombinations = [];
+  for (const path of paths) {
+    const contents = fs.readFileSync(path);
+    const hash = crypto.createHash("md5").update(contents).digest("hex");
+    pathHashCombinations.push({ path: path, hash: hash });
+  }
+
+  const duplicates = [];
+
+  for (const combination of pathHashCombinations) {
+    if (
+      pathHashCombinations.filter((x) => x.path === combination.path).length > 1 // TODO: maybe use any or something like that
+    ) {
+      duplicates.push(combination);
+    }
+  }
+
+  if (duplicates.length === 0) {
+    return "";
+  }
+
+  function compare(a, b) {
+    if (a.hash < b.hash) {
+      return -1;
+    }
+    if (a.hash > b.hash) {
+      return 1;
+    }
+    return 0;
+  }
+
+  duplicates.sort(compare);
+
+  let result = duplicates[0].path;
+  for (let i = 1; i < duplicates.length; i++) {
+    if (duplicates[i].path === duplicates[i - 1].path) {
+      result += `\n${duplicates[i].path}`;
+    } else {
+      result += `\n\n${duplicates[i].path}`;
+    }
+  }
+
+  return result;
+}
